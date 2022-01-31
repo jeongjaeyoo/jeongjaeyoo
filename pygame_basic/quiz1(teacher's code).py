7e8a6d914cabed6e16b1bb4ea261fc88f7c296c6
jeongjaeyoo <wjdwo503@naver.com>
2022-01-31 23:25:40 +0900

    second commit

--- pygame_basic/quiz1(teacher's code).py
+++ pygame_basic/quiz1(teacher's code).py
@@ -1,6 +1,14 @@
+from typing import Hashable
+from urllib.request import HTTPPasswordMgrWithPriorAuth
+
+
 asdfasdfasdfasddf
 
 
 print("Hello World!")
 
 print()
+
+Hashable
+HTTPPasswordMgrWithPriorAuth
+yeah 
